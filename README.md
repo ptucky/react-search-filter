b5eb756226543f4b088e9e8c681446312eec8c25
ptucky <w.ptucky@gmail.com>
2022-02-27 20:29:11 +0700

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# react-search-filter
-search and filter category
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm install`
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
+  const getData = async () => {
+    await axios
+      .get(`https://api.publicapis.org/categories`)
+      .then((response) => {
+        setData(response.data.categories);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const searchCategory = (value) => {
+    setSearchInput(value);
+    if (searchInput !== "") {
+      const filteredData = data.filter((item) => {
+        return Object.values(item)
+          .join("")
+          .toLowerCase()
+          .includes(searchInput.toLowerCase());
+      });
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(data);
+    }
+  };
+
+  return (
+    <div style={{ padding: 20 }}>
+      <input
+        type="text"
+        placeholder="Search..."
+        style={{ padding: "10px", width: "200px" }}
+        onChange={(e) => searchCategory(e.target.value)}
+      />
+
+      <div style={{ marginTop: 20 }}>
+        {searchInput.length > 1
+          ? filteredResults.map((item, key) => {
+              return (
+                <div key={key}>
+                  <span>{item}</span>
+                </div>
+              );
+            })
+          : data.map((item, key) => {
+              return (
+                <div key={key}>
+                  <span>{item}</span>
+                </div>
+              );
+            })}
+      </div>
+    </div>
+  );
+};
+
+export default App;
